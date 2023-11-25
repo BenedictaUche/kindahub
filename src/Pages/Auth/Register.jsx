c649575d3998dd98206659efb062cb5dfdfe8ea8
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-11-25 07:51:16 +0100

    added register page

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import AuthVectorImage from '../../assets/AuthVector.png';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='flex min-h-screen items-center bg-slate-50'>
+      <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <img src={AuthVectorImage} alt="Auth Vector" className="w-full h-auto" />
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
