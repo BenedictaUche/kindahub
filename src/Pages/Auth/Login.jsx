30e1b6be105fa91526bddb329f2153456bb414d7
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-11-28 06:34:36 +0100

    Added responsiveness to screens

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -4,15 +4,14 @@ import { signInWithEmailAndPassword } from 'firebase/auth';
 import auth from '../../firebase';
 import AuthVectorImage from '../../assets/AuthVector.png';
 
-
 const Login = () => {
-  const [formData, setFormData] = useState({email: '', password: ''});
+  const [formData, setFormData] = useState({ email: '', password: '' });
   const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value});
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
@@ -27,14 +26,13 @@ const Login = () => {
     }
   };
 
-
   return (
-    <div className='flex min-h-screen items-center bg-slate-50'>
+    <div className='flex flex-col md:flex-row min-h-screen bg-slate-50'>
       <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
         <img src={AuthVectorImage} alt="Auth Vector" className="w-full h-auto" />
       </div>
-      <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-8">
-        <h2 className="text-4xl font-bold mb-8">Welcome back to KindaHub</h2>
+      <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-8 flex flex-col justify-center">
+        <h2 className="text-4xl font-bold mb-8 text-center md:text-left">Welcome back to KindaHub</h2>
 
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
@@ -63,19 +61,19 @@ const Login = () => {
             />
           </div>
 
-          <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            className="bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
-          >
-            Login to KindaHub
-          </button>
-          <Link to="/forgot-password" className="text-blue-500">Forgot Password?</Link>
+          <div className="flex flex-col md:flex-row items-center justify-between">
+            <button
+              type="submit"
+              className="bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+            >
+              Login to KindaHub
+            </button>
+            <Link to="/forgot-password" className="text-blue-500 mt-4 md:mt-0 md:ml-4">Forgot Password?</Link>
           </div>
           {error && <p className="text-red-500">{error}</p>}
         </form>
 
-        <p className="mt-4">
+        <p className="mt-4 text-center md:text-left">
           Don't have an account? <Link to="/register" className="text-blue-500">Create a KindaHub Account</Link>
         </p>
       </div>
--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { updateProfile } from 'firebase/auth';
-import AuthVectorImage from "../../assets/AuthVector.png";
-import { Link, useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import auth from "../../firebase";
+import AuthVectorImage from '../../assets/AuthVector.png';
+import { Link, useNavigate } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import auth from '../../firebase';
 
 const Register = () => {
   const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    city: "",
-    password: "",
+    fullName: '',
+    email: '',
+    city: '',
+    password: '',
   });
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,13 +27,8 @@ const Register = () => {
 
       // Create user with additional data
       await createUserWithEmailAndPassword(auth, email, password);
-
-      // Optionally, update the user's profile with additional information
-      // (Note: This is just an example, and you may want to structure your user data differently)
       const user = auth.currentUser;
       await updateProfile(user, { displayName: fullName, cityCountry: city });
-
-      // You can store additional user data in Firestore or another database
 
       navigate('/home');
     } catch (signUpError) {
@@ -43,23 +38,16 @@ const Register = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center bg-slate-50">
+    <div className="flex flex-col md:flex-row min-h-screen bg-slate-50">
       <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2">
-        <img
-          src={AuthVectorImage}
-          alt="Auth Vector"
-          className="w-full h-auto"
-        />
+        <img src={AuthVectorImage} alt="Auth Vector" className="w-full h-auto" />
       </div>
-      <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-8">
-        <h2 className="text-4xl font-bold mb-8">Welcome to KindaHub</h2>
+      <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-8 flex flex-col justify-center">
+        <h2 className="text-4xl font-bold mb-8 text-center md:text-left">Welcome to KindaHub</h2>
 
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
-            <label
-              htmlFor="fullName"
-              className="block text-sm font-medium text-gray-600"
-            >
+            <label htmlFor="fullName" className="block text-sm font-medium text-gray-600">
               Full Name
             </label>
             <input
@@ -72,10 +60,7 @@ const Register = () => {
           </div>
 
           <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-600"
-            >
+            <label htmlFor="email" className="block text-sm font-medium text-gray-600">
               Email
             </label>
             <input
@@ -88,10 +73,7 @@ const Register = () => {
           </div>
 
           <div>
-            <label
-              htmlFor="city"
-              className="block text-sm font-medium text-gray-600"
-            >
+            <label htmlFor="city" className="block text-sm font-medium text-gray-600">
               City/Country
             </label>
             <input
@@ -104,10 +86,7 @@ const Register = () => {
           </div>
 
           <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-600"
-            >
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
               Password
             </label>
             <input
@@ -127,8 +106,8 @@ const Register = () => {
           </button>
         </form>
         {error && <p className="text-red-500">{error}</p>}
-        <p className="mt-4">
-          Already have an account?{" "}
+        <p className="mt-4 text-center md:text-left">
+          Already have an account?{' '}
           <Link to="/login" className="text-blue-500">
             Login
           </Link>
